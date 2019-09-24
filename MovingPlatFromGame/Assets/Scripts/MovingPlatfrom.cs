2c737d3755971f1785823a6083772fe6dd84286d
Talk2Sourabh <sourabh.singhai1990@gmail.com>
2019-09-24 12:40:40 +0530

    Project Setup

---
+++ MovingPlatFromGame/Assets/Scripts/MovingPlatfrom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatfrom : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _movingTarget1, _movingTarget2;
+    [SerializeField]
+    private float _speed = 1f;
+    bool _moveTarget1to2;
+    bool _moveTarget2to1;
+    void Update()
+    {
+        if (this.transform.position == _movingTarget1.position)
+        {
+            _moveTarget1to2 = true;
+            _moveTarget2to1 = false;
+        }
+        else if (this.transform.position == _movingTarget2.position)
+        {
+            _moveTarget1to2 = false;
+            _moveTarget2to1 = true;
+
+        }
+
+        if (_moveTarget1to2)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, _movingTarget2.position, Time.deltaTime * _speed);
+        }
+        else if (_moveTarget2to1)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, _movingTarget1.position, Time.deltaTime * _speed);
+        }
+    }
+}
